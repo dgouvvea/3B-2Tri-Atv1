df1f9ce274a5ba0f0c6889744f30fbdc6a3e0810
dgouvvea <81660341+dgouvvea@users.noreply.github.com>
2024-05-29 09:47:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var cxPrincipal = document.querySelector("cx-principal");
+var cxPerguntas = document.querySelector("cx-perguntas");
+var cxAlternativas = document.querySelector("cx-alternativas");
+var cxResultado = document.querySelector("cx-resultado");
+var txResultado = document.querySelector("tx-resultado");
